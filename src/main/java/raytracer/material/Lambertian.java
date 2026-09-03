b5517da2bd185f1cc266b446c05fb40a8c5168fb
El-Duckster <elena.fredenbrink@gmail.com>
2026-09-03 22:23:26 +0200

    chore:added more comments

--- src/main/java/raytracer/material/Lambertian.java
+++ src/main/java/raytracer/material/Lambertian.java
@@ -16,19 +16,20 @@ public final class Lambertian implements Material {
 
     public Lambertian(Color albedo) {
         this.albedo = albedo;
-    }
+    } // here start with the solidColor
 
     @Override
     public Color shade(Vector3D point, Vector3D normal, Scene scene) {
-        Color result = albedo.multiply(scene.ambient());
+        Color result = albedo.multiply(scene.ambient()); //start with the ambient light the starting color maybe a week light
 
+        // go through every light: For every light in the scene, calculate how much that light contributes to this point.
         for (PointLight light : scene.lights()) {
             if (scene.isOccluded(point, light.position())) {
-                continue; // something blocks this light -> shadow
+                continue; // Check whether the point is in shadow or something blocks this light -> shadow
             }
-            Vector3D lightDir = light.position().subtract(point).normalize();
-            double cosine = Math.max(0.0, normal.dot(lightDir)); //l = cos 0/degres
-            if (cosine == 0.0) { // 90 degrees
+            Vector3D lightDir = light.position().subtract(point).normalize(); //here i care about the direction so changes the vector's length to 1 while keeping its direction. Calculate how directly the light hits the surface, but never allow the brightness to go below zero.
+            double cosine = Math.max(0.0, normal.dot(lightDir)); //l = cos 0/degres = 1 , means it hits the object directly
+            if (cosine == 0.0) { // cos 90 degrees = 0 light arrow fron the side so it doesn't hitt the surface
                 continue;
             }
             result = result.add(
@@ -37,3 +38,26 @@ public final class Lambertian implements Material {
         return result;
     }
 }
+
+//        Start with a little ambient light.
+//
+//        For every lamp:
+//        Is the lamp blocked?
+//        YES → ignore it.
+//
+//        Find the direction from the point to the lamp.
+//
+//        Compare that direction with the surface normal.
+//
+//        Is the lamp behind / beside the surface?
+//        YES → ignore it.
+//
+//        Calculate:
+//        object color
+//        × light color
+//        × angle brightness
+//        × light intensity
+//
+//        Add that light to the final color.
+//
+//        Return the final color.
